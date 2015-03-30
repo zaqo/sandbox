34df66f725d471c393225b7e825cff81df94500c
Sergey Pavlov <zaqo@narod.ru>
2015-03-31 01:29:34 +0300

    init

--- db/migrate/20150325201250_create_shifts.rb
+++ db/migrate/20150325201250_create_shifts.rb
@@ -1,8 +1,8 @@
 class CreateShifts < ActiveRecord::Migration
   def change
     create_table :shifts do |t|
-      t.int :time
-      t.int :manager
+      t.string :time
+      t.string :manager
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330132139) do
+ActiveRecord::Schema.define(version: 20150330221518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 20150330132139) do
   end
 
   create_table "shifts", force: true do |t|
-    t.time     "time"
     t.string   "manager"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "time"
   end
 
   create_table "shotdowns", force: true do |t|
